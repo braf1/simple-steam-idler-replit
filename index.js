98131d2cc86c4aceb634caa97b3651a79caea029
braf1 <brafiiizz24@gmail.com>
2025-01-31 02:10:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570, 1942280, 603850, 700330, 1276390, 239140, 1280770, 20, 1172470, 444640, 363970, 666220, 70, 871720292030, 2772750, 2881650, 1238860, 361420, 398710, 518790, 714010, 2166060, 2909110, 10, 738060, 220, 320, 1353100];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
@@ -17,3 +17,19 @@ user.on('loggedOn', () => {
 	user.setPersona(status);               
 	user.gamesPlayed(games);
 });
+
+// var username2 = process.env.username2;
+// var password2 = process.env.password2;
+// var shared_secret2 = process.env.shared2;
+
+// var games2 = [730, 440, 570];  // Enter here AppIDs of the needed games
+// var status2 = 1;  // 1 - online, 7 - invisible
+
+
+// user2 = new steamUser();
+// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
+// user2.on('loggedOn', () => {
+// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
+// 	user2.setPersona(status2);               
+// 	user2.gamesPlayed(games2);
+// });
